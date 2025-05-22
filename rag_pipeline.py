8050a0a28741d4baf049e4f7840e5767a9236124
codiantdivyanshu <divyanshu.gupta@codiant.com>
2025-05-22 11:56:33 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
 from groq import Groq
 \
 load_dotenv()
